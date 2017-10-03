d7ea9c016f12e0224150d73bd4afb2a87c23d974
Terrence Wong <wongterrencew@gmail.com>
2017-10-03 16:44:43 +0200

    initial commit

---
+++ src/finden.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const commander = require('commander')
+const pkg = require('../package.json')
+const search = require('./search')
+
+commander.version(pkg.version)
+commander.parse(process.argv)
+
+const pattern = commander.args[0]
+
+search(pattern)
+  .catch(e => {
+    console.error(e)
+  })
---
+++ src/search.js
@@ -0,0 +1,54 @@
+const globby = require('globby')
+const mergeWith = require('lodash.mergewith')
+const { transformFile } = require('babel-core')
+const plugin = require('./babel-plugin-finden')
+const syntaxObjectRestSpread = require('babel-plugin-syntax-object-rest-spread')
+const syntaxClassProperties = require('babel-plugin-syntax-class-properties')
+const syntaxFlow = require('babel-plugin-syntax-flow')
+
+const DEFAULT_OPTIONS = {
+  ast: false,
+  babelrc: false,
+  code: false,
+  plugins: [
+  ]
+}
+const promisify = f =>
+  (...args) => {
+    return new Promise((resolve, reject) => {
+      const cb = (err, result) => {
+        if (err) reject(err)
+        resolve(result)
+      }
+      f(...args, cb)
+    })
+  }
+const transform = promisify(transformFile)
+
+module.exports = async function search (pattern) {
+  const paths = await globby(['**/*.js', '!node_modules/**'])
+  const filePromises = paths.map(path => {
+    const options = Object.assign({}, DEFAULT_OPTIONS, {
+      filename: path,
+      plugins: [
+        [ plugin, { pattern } ],
+        syntaxObjectRestSpread,
+        syntaxClassProperties,
+        syntaxFlow
+      ]
+    })
+    return transform(path, options)
+  })
+  const files = await Promise.all(filePromises)
+  const reports = files
+    .map(({ options }) => options.report)
+    .filter(report => report)
+    .reduce((consolidated, report ) => {
+      return mergeWith(
+        {},
+        consolidated,
+        report, (a, b) => Object.assign({}, a, b)
+      )
+    }, {})
+  console.log(JSON.stringify(reports, null, 2))
+}
